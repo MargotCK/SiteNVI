2a3c16a567698199ae4c25042cfbda97d4320392
nadia comuce <MargotCK>
2026-09-14 00:44:05 +0200

    Améliore le carrousel et le responsive de la page d'accueil

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OffreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,23 @@ use Symfony\Component\Routing\Attribute\Route;
 final class OffreController extends AbstractController
 {
     #[Route('/offres/{slug}', name: 'app_offre_show')]
-    public function show(string $slug): Response
-    {
-        return $this->render('offre/show.html.twig', [
+    public function show(
+        string $slug,
+        OffreRepository $offreRepository
+    ): Response {
+        $offre = $offreRepository->findOneBy([
             'slug' => $slug,
+            'actif' => true,
+        ]);
+
+        if (!$offre) {
+            throw $this->createNotFoundException(
+                'Cette offre n’existe pas ou n’est pas disponible.'
+            );
+        }
+
+        return $this->render('offre/show.html.twig', [
+            'offre' => $offre,
         ]);
     }
 }
